209ae78fbb8fa37b71ae50f488225f3e16d61602
phamhuyhoang97 <hoangtb31397@gmail.com>
2019-07-12 10:21:54 +0700

    update read file avsc on hdfs

--- src/main/java/vcc/project01/readavro/ReadAvroHDFS.java
+++ src/main/java/vcc/project01/readavro/ReadAvroHDFS.java
@@ -2,6 +2,8 @@ package vcc.project01.readavro;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
@@ -9,17 +11,31 @@ import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.mapred.FsInput;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class ReadAvroHDFS {
 	public static void main(String[] args) throws IOException {
 		
-		File schemaPath = new File("logObject.avsc");
-		Schema schema = new Schema.Parser().parse(schemaPath);
-		String folder = "hdfs://localhost:9000/topics/rest_test/partition=0/";
-		String file = "rest_test+0+0000005374+0000006373.avro";
-		String filePath = folder + file;
+		String fileAvsc = "logObject.avsc";
+		String folderAvsc = "hdfs://localhost:9000/topics/rest_test/";
+		String filePathAvsc = folderAvsc + fileAvsc;
 		Configuration conf = new Configuration();
+		conf.addResource("/usr/local/hadoop/etc/hadoop/core-site.xml");
+		conf.addResource("/usr/local/hadoop/etc/hadoop/hdfs-site.xml");
+		FileSystem fs = null;
+		try {
+			fs = FileSystem.get(new URI("hdfs://localhost:9000"), conf);
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		FSDataInputStream in = fs.open(new Path(filePathAvsc));
+		Schema schema = new Schema.Parser().parse(in);
+		String folder = "hdfs://localhost:9000/topics/rest_test/partition=0/";
+		String file = "rest_test+0+0000000000+0000000999.avro";
+		String filePath = folder + file;
 		FsInput avroData = new FsInput(new Path(filePath), conf);
 		
 		ReadAvoFile(avroData, schema, schema);
@@ -33,6 +49,4 @@ public class ReadAvroHDFS {
             System.out.println(dataFileReader.next());
         }
 	}
-	
-	
 }
--- src/main/java/vcc/project01/readavro/ReadAvroSpark.java
+++ src/main/java/vcc/project01/readavro/ReadAvroSpark.java
@@ -2,10 +2,13 @@ package vcc.project01.readavro;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.avro.Schema;
-import org.apache.avro.mapred.FsInput;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
@@ -23,8 +26,21 @@ public class ReadAvroSpark {
 				  .config("spark.master", "local")
 				  .getOrCreate();
 		
-		File schemaPath = new File("logObject.avsc");
-		Schema schema = new Schema.Parser().parse(schemaPath);
+		String fileAvsc = "logObject.avsc";
+		String folderAvsc = "hdfs://localhost:9000/topics/rest_test/";
+		String filePathAvsc = folderAvsc + fileAvsc;
+		Configuration conf = new Configuration();
+		conf.addResource("/usr/local/hadoop/etc/hadoop/core-site.xml");
+		conf.addResource("/usr/local/hadoop/etc/hadoop/hdfs-site.xml");
+		FileSystem fs = null;
+		try {
+			fs = FileSystem.get(new URI("hdfs://localhost:9000"), conf);
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		FSDataInputStream in = fs.open(new Path(filePathAvsc));
+		Schema schema = new Schema.Parser().parse(in);
 		
 		String folder = "hdfs://localhost:9000/topics/rest_test/partition=0/";
 		String file = "*";
@@ -35,8 +51,8 @@ public class ReadAvroSpark {
 				.option("avroSchema", schema.toString())
 				.load(filePath);
 		usersDF.createOrReplaceTempView("logs");
-		Dataset<Row> temDF = spark.sql("SELECT count(d) FROM logs WHERE d = 'null'");
-		temDF.show();
+		Dataset<Row> temDF = spark.sql("SELECT * FROM logs WHERE b = 'a'");
+		temDF.show(100, false);
 		
 		spark.stop();
 	}
